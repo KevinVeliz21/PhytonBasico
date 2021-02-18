8c48b07cdace057a27bc1585603564f52948e81b
Kevin <3033872890108@ingenieria.usac.edu.gt>
2021-02-18 14:47:14 -0600

    Hoja1

---
+++ Hoja1.py
@@ -0,0 +1,6 @@
+Peso=input("Escriba su peso en Kilogramos      ")
+Estatura=input("Escriba su estatura en metros   ")
+imc = round(float(Peso)/(float(Estatura)*float(Estatura)),2)
+print("Su masa corporal es"        +     str(imc))
+
+
